8b2c3f147421723e79dde17f823e85610112fa34
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-11-08 16:50:15 +0100

    refactor: recloned laravel-auth repository

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,16 @@ use App\Http\Controllers\Admin\ProjectController as AdminProjectController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/admin/projects', [AdminProjectController::class, 'index'])->name('Admin.projects.index');
-Route::get('/admin/projects/create', [AdminProjectController::class, 'create'])->name('Admin.projects.create');
-Route::get('/admin/projects/edit{id}', [AdminProjectController::class, 'edit'])->name('Admin.projects.edit');
-Route::get('/admin/projects/{id}', [AdminProjectController::class, 'show'])->name('Admin.projects.show');
-Route::put('/admin/projects/{id}', [AdminProjectController::class, 'update'])->name('Admin.projects.update');
-Route::post('/admin/projects', [AdminProjectController::class, 'store'])->name('Admin.projects.store');
-Route::delete('/admin/projects/{id}', [AdminProjectController::class, 'destroy'])->name('Admin.projects.delete');
 
+Route::prefix('/admin')->name('Admin.')->group(function () {
+    Route::get('/projects', [AdminProjectController::class, 'index'])->name('projects.index');
+    Route::get('/projects/create', [AdminProjectController::class, 'create'])->name('projects.create');
+    Route::get('/projects/edit{id}', [AdminProjectController::class, 'edit'])->name('projects.edit');
+    Route::get('/projects/{id}', [AdminProjectController::class, 'show'])->name('projects.show');
+    Route::put('/projects/{id}', [AdminProjectController::class, 'update'])->name('projects.update');
+    Route::post('/projects', [AdminProjectController::class, 'store'])->name('projects.store');
+    Route::delete('/projects/{id}', [AdminProjectController::class, 'destroy'])->name('projects.delete');
+});
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
