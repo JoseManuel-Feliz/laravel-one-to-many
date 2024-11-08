ee398f41abb994324440c29064e5afe2bf4958b3
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-11-08 18:00:54 +0100

    feat: remove fillable in to project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,13 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
-
-    protected $fillable = [
-        'title',
-        'status',
-        'author',
-        'contributors',
-        'project_start_date',
-        'description'
-    ];
 }
